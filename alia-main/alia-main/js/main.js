bc9e70b09641f5a9fd6313ab73e2195825bb7dc2
Eng \ elsayed ebed <123215092+elsayedebed@users.noreply.github.com>
2023-05-26 16:52:52 +0300

    Add files via upload

---
+++ alia-main/alia-main/js/main.js
@@ -0,0 +1,78 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
+// scrooll animation
+const sr= ScrollReveal({
+    origin:'top' ,
+    distance:'60px',
+    duration:2500,
+    delay:400,
+})
+sr.reveal(`.home`)
+sr.reveal(`.home,.about__group `,{delay:700,origin:'bottom'})
+sr.reveal(`.logos__img, .review, .arrow `,{interval:100})
+sr.reveal(`.blogs,.about__description ,.footer`,{origin:'left'})
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop:true,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        600:{
+            slidesPerView:2,
+        },
+    },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        991:{
+            slidesPerView:3,
+        },
+    },
+});
